56902876de093e1f957e4d6be0fe8cc2b2a6723d
Simone Cheah <simone_cheah@hotmail.com>
2020-12-07 04:51:29 +1100

    created workout model to export

---
+++ models/workoutModel.js
@@ -0,0 +1,23 @@
+const mongoose = require( "mongoose" );
+const { Schema } = mongoose;
+
+const WorkoutSchema = new Schema( {
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: { type: String, trim: true },
+            name: { type: String, trim: true },
+            duration: Number,
+            weight: Number,
+            reps: Number,
+            sets: Number
+        }
+    ]
+} );
+
+const Workout = mongoose.model( "Workout", WorkoutSchema );
+
+module.exports = Workout;
