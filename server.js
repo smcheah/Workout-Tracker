cd62a12c6c4439d638a2358a496b4367dafcec01
Simone Cheah <simone_cheah@hotmail.com>
2020-12-07 04:52:51 +1100

    add initial server file

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require( "express" );
+const logger = require( "morgan" );
+const mongoose = require( "mongoose" );
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.use( logger( "dev" ) );
+app.use( express.urlencoded( { extended: true } ) );
+app.use( express.json() );
+app.use( express.static( "public" ) );
+
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+} );
+
+require( './controllers/html-routes.js' )( app );
+require( './controllers/api-routes.js' )( app );
+
+app.listen( PORT, () => {
+    console.log( `App running on port ${PORT}!` );
+} );
