cef4b768e3f3719d285150e1453c9ecc33a3c8fa
Simone Cheah <simone_cheah@hotmail.com>
2020-12-08 04:01:09 +1100

    fixed db import and added flag

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,9 +1,10 @@
 let mongoose = require( "mongoose" );
-let db = require( "../models" );
+let Workout = require( "../models/workoutModel.js" );
 
 mongoose.connect( "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 } );
 
 let workoutSeed = [
@@ -135,13 +136,13 @@ let workoutSeed = [
   }
 ];
 
-// db.Workout.deleteMany( {} )
-//   .then( () => db.Workout.collection.insertMany( workoutSeed ) )
-//   .then( data => {
-//     console.log( data.result.n + " records inserted!" );
-//     process.exit( 0 );
-//   } )
-//   .catch( err => {
-//     console.error( err );
-//     process.exit( 1 );
-//   } );
+Workout.deleteMany( {} )
+  .then( () => Workout.collection.insertMany( workoutSeed ) )
+  .then( data => {
+    console.log( data.result.n + " records inserted!" );
+    process.exit( 0 );
+  } )
+  .catch( err => {
+    console.error( err );
+    process.exit( 1 );
+  } );
