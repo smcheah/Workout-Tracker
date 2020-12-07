76fb7e481e0de03efb04610a3285253567cc31f0
Simone Cheah <simone_cheah@hotmail.com>
2020-12-08 04:01:55 +1100

    fixed routes to post and get data

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -2,12 +2,45 @@ const Workout = require( '../models/workoutModel.js' );
 
 module.exports = function ( app ) {
     app.get( '/api/workouts', ( req, res ) => {
+        Workout
+            .find( {} )
+            .then( data => {
+                res.json( data );
+            } )
+            .catch( err => {
+                res.json( err );
+            } );
+    } );
+
+    app.get( '/api/workouts/:id', ( req, res ) => {
+        Workout
+            .findById( req.params.id )
+            .then( data => {
+                res.json( data );
+            } )
+            .catch( err => {
+                res.json( err );
+            } );
+    } );
+
+    app.post( '/api/workouts', ( req, res ) => {
+        Workout
+            .create( req.body )
+            .then( data => {
+                res.json( data );
+            } )
+            .catch( err => {
+                res.json( err );
+            } );
+    } );
+
+    app.get( '/api/workouts/range', ( req, res ) => {
         Workout.find( {} )
             .then( data => {
                 res.json( data );
             } )
             .catch( err => {
-                throw err;
+                res.json( err );
             } );
     } );
 };
