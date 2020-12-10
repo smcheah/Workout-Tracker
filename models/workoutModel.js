92250f5f231484ac337a53f8ca24d8449d014b68
Simone Cheah <simone_cheah@hotmail.com>
2020-12-11 00:16:48 +1100

    return totalduration

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -2,21 +2,32 @@ const mongoose = require( "mongoose" );
 const { Schema } = mongoose;
 
 const WorkoutSchema = new Schema( {
-    day: {
-        type: Date,
-        default: Date.now
-    },
-    exercises: [
-        {
-            type: { type: String, trim: true },
-            name: { type: String, trim: true },
-            duration: Number,
-            weight: Number,
-            reps: Number,
-            sets: Number
-        }
-    ]
+	day: {
+		type: Date,
+		default: Date.now,
+	},
+	exercises: [
+		{
+			type: { type: String, trim: true },
+			name: { type: String, trim: true },
+			duration: Number,
+			weight: Number,
+			reps: Number,
+			sets: Number,
+		},
+	],
+	totalDuration: { type: Number, default: function() {
+		return this.getTotalDuration()
+	} }
 } );
+
+WorkoutSchema.methods.getTotalDuration = function () {
+	let total = 0;
+	for (let item of this.exercises) {
+		total += item.duration
+	}
+    return total
+}
 
 const Workout = mongoose.model( "Workout", WorkoutSchema );
 
